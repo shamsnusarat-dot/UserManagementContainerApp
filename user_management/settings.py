e2664841cd82d4b17d336d151869dc24dcb2d8b3
Shiekshamsnusarat Nazeerahamed <shiekshamsnusarat.nazeerahamed@kumaran.com>
2026-04-29 05:54:09 -0700

    SQLite Setup

--- user_management/settings.py
+++ user_management/settings.py
@@ -36,7 +36,10 @@ CSRF_TRUSTED_ORIGINS = [
 ]
 
 ALLOWED_HOSTS = [
-    "django-demo-app-cnfeddg9f0e7ckec.southindia-01.azurewebsites.net"
+    "django-demo-app-cnfeddg9f0e7ckec.southindia-01.azurewebsites.net",
+    "127.0.0.1",
+    "localhost",
+    "*"
 ]
 
 
@@ -87,24 +90,10 @@ WSGI_APPLICATION = 'user_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.getenv('DB_NAME', 'postgres'),
-#         'USER': os.getenv('DB_USER', 'postgres'),
-#         'PASSWORD': os.getenv('DB_PASSWORD', ''),
-#         'HOST': os.getenv('DB_HOST', 'localhost'),
-#         'PORT': os.getenv('DB_PORT', '5432'),
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', 'postgres'),
-        'USER': os.getenv('DB_USER', 'postgres'),
-        'PASSWORD': os.getenv('DB_PASSWORD', 'postgres'),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '5432'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
