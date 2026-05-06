e398cfc428dd38638e104844c7f3e3bba3340b6c
Shiekshamsnusarat Nazeerahamed <shiekshamsnusarat.nazeerahamed@kumaran.com>
2026-05-06 18:21:14 +0530

    Redirection Refractor

--- users/auth_urls.py
+++ users/auth_urls.py
@@ -4,6 +4,6 @@ from .auth_views import register
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='users/login.html'), name='ms-login'),
-    path('logout/', LogoutView.as_view(), name='ms-logout'),
+    path('logout/', LogoutView.as_view(next_page='/auth/login/'), name='ms-logout'),
     path('register/', register, name='register'),
 ]
--- users/urls.py
+++ users/urls.py
@@ -6,11 +6,13 @@ from .views import (
     UserEditTemplateView, UserDeleteTemplateView,
 )
 
+login = login_required(login_url='/auth/login/')
+
 urlpatterns = [
-    path('', login_required(UserListTemplateView.as_view()), name='user-list-template'),
-    path('create/', login_required(UserCreateTemplateView.as_view()), name='user-create-template'),
-    path('<int:pk>/edit/', login_required(UserEditTemplateView.as_view()), name='user-edit-template'),
-    path('<int:pk>/delete/', login_required(UserDeleteTemplateView.as_view()), name='user-delete-template'),
+    path('', login(UserListTemplateView.as_view()), name='user-list-template'),
+    path('create/', login(UserCreateTemplateView.as_view()), name='user-create-template'),
+    path('<int:pk>/edit/', login(UserEditTemplateView.as_view()), name='user-edit-template'),
+    path('<int:pk>/delete/', login(UserDeleteTemplateView.as_view()), name='user-delete-template'),
 
     path('api/users/', UserListCreateView.as_view(), name='user-list-create'),
     path('api/users/<int:pk>/', UserDetailView.as_view(), name='user-detail'),
